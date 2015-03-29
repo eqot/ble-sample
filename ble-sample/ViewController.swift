b2cd0708e737d5c421ca5e0810a3fc2916c6cd6a
Ikuo Terado <eqobar@gmail.com>
2015-03-29 13:13:34 +0900

    Initial Commit

---
+++ ble-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ble-sample
+//
+//  Created by eqo on 2015/03/29.
+//  Copyright (c) 2015年 eqot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ble-sampleTests/ble_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  ble_sampleTests.swift
+//  ble-sampleTests
+//
+//  Created by eqo on 2015/03/29.
+//  Copyright (c) 2015年 eqot. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ble_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
